6b4e0cbbb87a85ce45962aa7af95db42c7fbbf37
jesterb0206 <jesterb@seattleu.edu>
2023-01-30 20:56:47 -0800

    Teesting branch protection rule

--- client/src/components/Exercise.js
+++ client/src/components/Exercise.js
@@ -1,35 +1,18 @@
+import {React, useState} from 'react';
 
-import { React, useState} from 'react'
-
-
-const styles = {
-
-
-
-}
-
-
+const styles = {};
 
 const Exercise = () => {
-
-    return (
-
-    <div className = "d-flex justify-content-center">
-        <button> - remove exercise</button>
-        <h1> Example exercise 1</h1>
-        <p>Weight</p>
-        <button> + used to increase set</button>
-        <button> - used to decrease set</button>
-        
+  return (
+    <div className='d-flex justify-content-center'>
+      {/* Testing Comment*/}
+      <button> - remove exercise</button>
+      <h1> Example exercise 1</h1>
+      <p>Weight</p>
+      <button> + used to increase set</button>
+      <button> - used to decrease set</button>
     </div>
-    )
-}
+  );
+};
 
-
-export default Exercise
-
-
-
-
-
-
+export default Exercise;
