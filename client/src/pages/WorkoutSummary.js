30fc6e6486362e5747c84631a4795911ceeea76c
Isaiah411013 <Archangel_r320@hotmail.com>
2023-02-01 01:00:43 -0800

    added summary querys and mutations /w starter code in workoutSummary.js

--- client/src/pages/WorkoutSummary.js
+++ client/src/pages/WorkoutSummary.js
@@ -8,6 +8,19 @@ Each card will display a date and the workouts, and the records for the workouts
 -card
 --list of workouts
 --number of reps and sets done
-
-
 */
+import React from 'react';
+import { useQuery } from '@apollo/client';
+import { QUERY_WORKOUT } from '../utils/mutations'
+
+export default function WorkoutSummary() {
+    const { loading, data } = useQuery(QUERY_WORKOUT);
+    const workouts = data?.workout || [];
+
+
+    return (
+        <div>
+
+        </div>
+    )
+}
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -2,20 +2,33 @@ import { gql } from '@apollo/client';
 
 
 export const QUERY_ME = gql`
-    query me {
-        me {
-          _id
-          username
-          email
-          savedBooks{
-            authors
-            bookId
-            description
-            title
-            image
-            link
-          }
-        }
-    }`
+  query me {
+    me {
+      _id
+      username
+      email
+      savedBooks{
+        authors
+        bookId
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`
 
-
+export const QUERY_WORKOUT = gql`
+  query getWorkout{
+    workout{
+      dayOfTheWeek
+      exerciseName
+      workoutId
+      weight
+      sets
+      reps
+      other
+    }
+  }
+`
