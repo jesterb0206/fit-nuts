ce0c384beefe585448c7ec364f328b430cbbdb72
jesterb0206 <jesterb@seattleu.edu>
2023-02-01 19:03:45 -0800

    Merged Pat & I's Work Together

--- client/src/pages/WorkoutSummary.js
+++ client/src/pages/WorkoutSummary.js
@@ -1,10 +1,14 @@
 /* eslint-disable react/jsx-no-undef */
 
 import React from 'react';
+import {useQuery} from '@apollo/client';
+import {QUERY_WORKOUT} from '../utils/mutations';
 import Card from '.././components/Card';
 import '../App.css';
 
 function WorkoutSummary() {
+  const {loading, data} = useQuery(QUERY_WORKOUT);
+  const workouts = data?.workout || [];
   return (
     <div ClassName='workout__summary'>
       <h1 id='center__text'>Workout Summary</h1>
@@ -21,26 +25,4 @@ function WorkoutSummary() {
   );
 }
 
-
--likes, comments
--date
--card
---list of workouts
---number of reps and sets done
-*/
-import React from 'react';
-import { useQuery } from '@apollo/client';
-import { QUERY_WORKOUT } from '../utils/mutations'
-
-export default function WorkoutSummary() {
-    const { loading, data } = useQuery(QUERY_WORKOUT);
-    const workouts = data?.workout || [];
-
-
-    return (
-        <div>
-
-        </div>
-    )
-}
-
+export default WorkoutSummary;
