98a4e1cd7afdffa9d07598a8cf97a45ebc3133ee
Isaiah411013 <Archangel_r320@hotmail.com>
2023-02-02 21:33:13 -0800

    updating workoutSummary forr population of cards

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,30 +1,41 @@
 import React from 'react';
 import '../App.css';
+import {useQuery} from '@apollo/client';
+import {QUERY_WORKOUT} from '../utils/queries'
 
-function Card() {
+
+
+const Card = ({dayOfTheWeek}) => {
+  
+  const {loading, data} = useQuery( QUERY_WORKOUT, {
+    variables: {dayOfTheWeek},
+  });
+  const workouts = data?.workout || [];
+
   return (
     <div className='card'>
-      <h1 id='center__text'>Monday</h1>
+      <h1 id='center__text'>{dayOfTheWeek}</h1>
+      {/* {workout.map((workout) */}
       <table>
-        <tr>
-          <th>Workout</th>
-          <th>Weight</th>
-          <th>Sets</th>
-          <th>Reps</th>
-        </tr>
-        <tr>
-          <td>Barbell back squat</td>
-          <td>141 lb</td>
-          <td>5</td>
-          <td>5</td>
-        </tr>
-        <tr>
-          <td>Dumbbell Lateral Raise</td>
-          <td>34 lb</td>
-          <td>2</td>
-          <td>8</td>
-        </tr>
-      </table>
+          <tr>
+            <th>Workout</th>
+            <th>Weight</th>
+            <th>Sets</th>
+            <th>Reps</th>
+          </tr>
+          <tr>
+            <td>something cool</td>
+            <td>141 lb</td>
+            <td>5</td>
+            <td>5</td>
+          </tr>
+          <tr>
+            <td>Dumbbell Lateral Raise</td>
+            <td>34 lb</td>
+            <td>2</td>
+            <td>8</td>
+          </tr>
+        </table>
       <button>Add/Change Workout</button>
     </div>
   );
