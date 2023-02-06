bd3c7bc514d87dcb7d4864bcc3f1d618070b6dcc
jesterb0206 <jesterb@seattleu.edu>
2023-02-05 22:14:06 -0800

    Caught Some Grammar Errors in the Home Page Paragraph

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -10,14 +10,14 @@ const Homepage = () => {
       <h1 className='welcomeHeader'>Welcome To Fit NUTS! 🏋️‍♀️🥜🏋️‍♂️</h1>
       {/* Fit NUTS Blurb */}
       <p className='introParagraph'>
-        This web application is for the modern day fitness nut! Here you will be
-        able to save all of your daily exercises, workouts, reps, sets, etc. All
+        This web application is for the modern day fitness nut! Here you'll be
+        able to save all your daily exercises, workouts, reps, sets, etc. All
         you have to do is sign in or create an account and get to pumping some
         iron. Once you’re signed in, you can start creating workouts by
         selecting the “create workout” button in the navigation bar. You can
-        view your past workouts by selecting the “workout summary” button. This
-        also where you can find your last PR (Personal Record). Our goal is to
-        help you to keep track of your progress and motivate you to beat your
+        then view your past workouts by selecting the “workout summary” button.
+        This is also where you can find your last PR (Personal Record). Our goal
+        is to help you keep track of your progress and motivate you to beat your
         personal best. I’ll see you at the gym and let’s get to exercising!
       </p>
     </div>
